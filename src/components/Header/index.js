8cc036e82798dfe5204bcead23da0ea707c07e57
Juan Menezes <99377147+JuanMLima@users.noreply.github.com>
2022-09-06 20:14:47 -0300

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ export default ({black}) => {
         <header className={black ? 'black' : ''}>
             <div className="header--logo">
                 <a href="/">
-                    <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"></img>
+                    <img src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo.png"></img>
                 </a>
             </div>
             <div className="header--user">
